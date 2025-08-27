3fe92b6898284f9c2dfffef281682afc27fcd286
OjiChanERO <151846161+OjiChanERO@users.noreply.github.com>
2025-08-27 10:52:30 +0930

    Create state.js

---
+++ js/state.js
@@ -0,0 +1,11 @@
+// js/state.js
+const state = {
+    user: null,
+    profile: null,
+    projects: [],
+    capabilities: [],
+    editingProjectId: null,
+    editingCapabilityId: null,
+};
+
+export default state;
