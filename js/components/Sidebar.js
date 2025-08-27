70531565042b915667a3d9a29ca2e34657f1dc6c
OjiChanERO <151846161+OjiChanERO@users.noreply.github.com>
2025-08-27 10:54:55 +0930

    Create Sidebar.js

---
+++ js/components/Sidebar.js
@@ -0,0 +1,33 @@
+export function renderSidebar(activeRoute) {
+    const container = document.createElement('aside');
+    container.className = 'menu p-4 glass-effect';
+    container.innerHTML = `
+        <p class="menu-label">
+            Manage
+        </p>
+        <ul class="menu-list">
+            <li>
+                <a href="#/dashboard" class="${activeRoute === '#/dashboard' ? 'is-active' : ''}">
+                    <span class="icon-text">
+                        <span class="icon"><i class="fas fa-edit"></i></span>
+                        <span>Content Dashboard</span>
+                    </span>
+                </a>
+            </li>
+        </ul>
+        <p class="menu-label">
+            Visualize
+        </p>
+        <ul class="menu-list">
+            <li>
+                <a href="#/skillmap" class="${activeRoute === '#/skillmap' ? 'is-active' : ''}">
+                    <span class="icon-text">
+                        <span class="icon"><i class="fas fa-project-diagram"></i></span>
+                        <span>Skill Map</span>
+                    </span>
+                </a>
+            </li>
+        </ul>
+    `;
+    return container;
+}
